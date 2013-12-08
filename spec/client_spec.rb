aaeb31789bcae3944a58e94e9df760cacd9ae798
Janis Miezitis <janjiss@gmail.com>
2013-12-08 23:47:40 +0200

    Remved duplicationw

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -20,25 +20,27 @@ describe ShaCache::Client do
       end
     end
 
+    subject { ShaCache::Client }
+
     it "writes data and converts it's contents to sha" do
       body = "response from 3rd pary API"
       body_sha = convert_to_sha(body)
       expect(@adapter).to receive(:write_data).with("key", body_sha)
-      ShaCache::Client.write_data("key", body)
+      subject.write_data("key", body)
     end
 
-    it "checks if data has allready been used" do
+    it "checks if data has allready been used and returns true if it was" do
       body = "response from 3rd pary API"
       body_sha = convert_to_sha(body)
       expect(@adapter).to receive(:get_data_by_key).with("key").and_return(body_sha)
-      expect(ShaCache::Client.data_was_used?("key", body)).to be_true
+      expect(subject.data_was_used?("key", body)).to be_true
     end
 
-    it "checks if data has allready been used" do
+    it "checks if data has allready been used and returns false if it was not" do
       body = "response from 3rd pary API"
       body_sha = convert_to_sha(body)
       expect(@adapter).to receive(:get_data_by_key).with("key").and_return("other_sha_retured")
-      expect(ShaCache::Client.data_was_used?("key", body)).to be_false
+      expect(subject.data_was_used?("key", body)).to be_false
     end
   end
 end
